2de8ca8622e3739f6da0a703322720e3471288d0
mortarmaster3000 <wmotzenbecker@Newpaltz.k12.ny.us>
2026-03-11 16:59:52 +0000

    a

--- script.js
+++ script.js
@@ -18,6 +18,14 @@ const diabetes = document.getElementById("diabetes");
 const diabetesshopamt = document.getElementById("diabetesshopamt");
 const diabetesprice = document.getElementById("diabetesprice");
 
+const nuclear = document.getElementById("nuclear");
+const nuclearshopamt = document.getElementById("nuclearshopamt");
+const nuclearprice = document.getElementById("nuclearprice");
+
+const cyber = document.getElementById("cyber");
+const cybershopamt = document.getElementById("cybershopamt");
+const cyberprice = document.getElementById("cyberprice");
+
 const secretbutton = document.getElementById("secretbutton");
 
 let count = 0
@@ -25,16 +33,22 @@ let Lamt = 0
 let Camt = 0
 let Clamt = 0
 let Damt = 0
+let Namt = 0
+let Cyamt = 0
 
 let childMultiply = 0
 let laborMultiply = 0
 let clankerMultiply = 0
 let diabetesMultiply = 0
+let nuclearMultiply = 0
+let cyberMultiply = 0
 
 let priceoflabor = 50
 let priceofchild = 10
 let priceofclanker = 125
 let priceofdiabetes = 500
+let priceofnuclear = 750
+let priceofcyber = 1250
 
 function clickButton() {
     if (Damt >= 1) {
@@ -206,6 +220,82 @@ function buyDiabetes() {
 };
 diabetes.addEventListener("click", buyDiabetes)
 
+function buyNuclear() {
+
+    if (count >= priceofnuclear) {
+    Namt += 1; 
+    nuclearshopamt.textContent = Namt;
+    count -= priceofnuclear;
+    document.getElementById("nuclearprice").textContent = priceofnuclear;
+
+    if (Namt < 5) {
+        nuclearMultiply = 1.55;
+    } else if (Namt < 10) {
+        nuclearMultiply = 1.3;
+    } else if (Namt < 15) {
+        nuclearMultiply = 1.2
+    } else if (Namt < 20) {
+        nuclearMultiply = 1.1
+    } else if (Namt < 25) {
+        nuclearMultiply = 1.05
+    } else if (Namt > 50) {
+        nuclearMultiply = 1.02
+    } else if (Namt > 75) {
+        nuclearMultiply = 1.01
+    } else if (Namt > 100) {
+        nuclearMultiply = 1.05
+    } else if (Namt > 125) {
+        nuclearMultiply = 1.02
+    }
+     else if (Namt > 150) {
+        nuclearMultiply = 1.001
+    }
+    priceofnuclear = Math.floor(priceofnuclear * nuclearMultiply);
+    nuclearprice.textContent = priceofnuclear;
+    } else {
+
+    }
+};
+nuclear.addEventListener("click", buyNuclear);
+
+function buyCyberTrump() {
+
+    if (count >= priceofcyber) {
+    Cyamt += 1; 
+    cybershopamt.textContent = Cyamt;
+    count -= priceofcyber;
+    document.getElementById("cyberprice").textContent = priceofcyber;
+
+    if (Cyamt < 5) {
+        cyberMultiply = 1.55;
+    } else if (Cyamt < 10) {
+        cyberMultiply = 1.3;
+    } else if (Cyamt < 15) {
+        cyberMultiply = 1.2
+    } else if (Cyamt < 20) {
+        cyberMultiply = 1.1
+    } else if (Cyamt < 25) {
+        cyberMultiply = 1.05
+    } else if (Cyamt > 50) {
+        cyberMultiply = 1.02
+    } else if (Cyamt > 75) {
+        cyberMultiply = 1.01
+    } else if (Cyamt > 100) {
+        cyberMultiply = 1.05
+    } else if (Cyamt > 125) {
+        cyberMultiply = 1.02
+    }
+     else if (Cyamt > 150) {
+        cyberMultiply = 1.001
+    }
+    priceofcyber = Math.floor(priceofcyber * cyberMultiply);
+    cyberprice.textContent = priceofcyber;
+    } else {
+
+    }
+};
+cyber.addEventListener("click", buyCyberTrump);
+
 function autoChild() {
     count += Camt * 0.005;
     number.textContent = Math.floor(count);
@@ -218,6 +308,16 @@ function autoLabor() {
 
 function autoClanker() {
     count += Clamt * 0.05;
+    number.textContent = Math.floor(count);
+}
+
+function autoNuclear() {
+    count += Namt * 0.15;
+    number.textContent = Math.floor(count);
+}
+
+function autoCyber() {
+    count += Cyamt * 0.25;
     number.textContent = Math.floor(count);
 }
 
@@ -241,10 +341,23 @@ function updateShop() {
         diabetes.style.opacity = "1";
     } else {
         diabetes.style.opacity = "0.5";
-    }
-}
+    }     if (count >= priceofnuclear) {
+        nuclear.style.opacity = "1";
+    } else {
+        nuclear.style.opacity = "0.5";
+    }     if (count >= priceofcyber) {
+        cyber.style.opacity = "1";
+    } else {
+        cyber.style.opacity = "0.5";
+    } 
+    
+
+    
+};
 
 setInterval(updateShop, 10);
 setInterval(autoChild, 10);
 setInterval(autoLabor, 10);
 setInterval(autoClanker, 10);
+setInterval(autoNuclear, 10);
+setInterval(autoCyber, 10);
